04f1ac703eb833e23bc65e9a7ac4516bdb4e026a
salt-dev <salt-dev@salt-devs-MacBook-Pro-3.local>
2023-02-21 16:43:12 +0100

    rendering 10 photos

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,82 @@
-import './styles/main.scss';
-// Create a class property without a constructor
-class Game {
-    name = 'Violin Charades'
-  }
-  const myGame = new Game()
-  // Create paragraph node
-  const p = document.createElement('p')
-  p.textContent = `I like ${myGame.name}.`
+import "./styles/main.scss";
+// Create form
+const form = document.createElement("form");
+const input = document.createElement("input");
+const input2 = document.createElement("input");
 
+input.setAttribute("type", "text");
+input.setAttribute("id", "iid");
+input2.setAttribute("type", "submit");
+input2.setAttribute("value", "Submit");
 
 // Create heading node
-const heading = document.createElement('h1')
-heading.textContent = 'It works!'
+const heading = document.createElement("h1");
+heading.textContent = "It works!";
 
 // Append heading node to the DOM
-const app = document.querySelector('#root')
-app?.append(heading)
+const app = document.querySelector("#root");
+app?.append(form);
+form.appendChild(input);
+form.appendChild(input2);
+
+const getImage = () => {
+
+
+
+
+  input2.addEventListener("submit", () => {
+   
+  });
+form.addEventListener("submit", (event)=>{
+  event.preventDefault()
+  
+  fetch(
+    `https://api.unsplash.com/search/photos?client_id=u5bAUNTIlopz0D5NOD-mPjOdb-p7SXjvkS_xO76UEvE&query=${input.value}`
+  ).then(response => response.json())
+  .then(response => {
+    
+
+
+response.results.forEach(item  => {
+  const imageEl = document.createElement("img")
+  imageEl.setAttribute('src',`${item.urls.small}`)
+
+app?.appendChild(imageEl);
+});
+  })
+})
+
+
+
+
+
+  // .then(result => console.log(1, result));
+  // .then(response => console.log(2, typeof(response)))
+};
+
+// function getImage() {
+//   return new Promise((resolve, reject) => {
+//     fetch(
+//       `https://api.unsplash.com/search/photos?client_id=u5bAUNTIlopz0D5NOD-mPjOdb-p7SXjvkS_xO76UEvE&query=cats`
+//     )
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((response) => {
+//         let data = response.results;
+//         resolve(data);
+        
+//       });
+//   });
+// }
+
+
+
+const watchForm = () => {
+  input2.addEventListener("submit", () => {
+    getImage();
+  });
+}; 
+
+watchForm();
+getImage();
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, './src/index.ts'),
+    main: path.resolve(__dirname, "./src/index.ts"),
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.bundle.js",
   },
   module: {
     rules: [
@@ -17,23 +17,23 @@ module.exports = {
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
       // Images
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
       },
     ],
   },
-  mode: 'development',
+  mode: "development",
   devServer: {
     historyApiFallback: true,
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, "./dist"),
     open: true,
     compress: true,
     hot: true,
@@ -41,11 +41,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
-      template: path.resolve(__dirname, './src/template.html'), // template file
-      filename: 'index.html', // output file
+      title: "Bug-Busters",
+      template: path.resolve(__dirname, "./src/template.html"), // template file
+      filename: "index.html", // output file
     }),
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
   ],
-}
+};
