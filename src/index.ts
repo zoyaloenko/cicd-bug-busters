9f9d3d05d8bab2a3b535eb55671242b0592cf618
salt-dev <salt-dev@salt-devs-MacBook-Pro-7.local>
2023-02-22 11:57:56 +0100

    created a dropdown menu of previous searches

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,24 @@ const form = document.createElement("form");
 const input = document.createElement("input");
 const input2 = document.createElement("input");
 const container = document.createElement("div");
+const datalist = document.createElement("datalist");
+const option = document.createElement("option");
 
 input.setAttribute("type", "text");
 input.setAttribute("id", "iid");
 input2.setAttribute("type", "submit");
 input2.setAttribute("value", "Submit");
+// option.setAttribute("value", "deann");
+
+JSON.parse(localStorage.getItem("searchHistory")).forEach((item) => {
+  const option = document.createElement("option");
+  option.setAttribute("value", item);
+  datalist.appendChild(option);
+});
+
+input.setAttribute("list", "dropdown");
+datalist.setAttribute("id", "dropdown");
+input.setAttribute("autocomplete", "off");
 
 // Append heading node to the DOM
 const app = document.querySelector("#root");
@@ -16,11 +29,23 @@ app?.append(form);
 form.appendChild(input);
 form.appendChild(input2);
 app?.appendChild(container);
+input.appendChild(datalist);
+
+const toLocalStorage = (arg) => {
+  const searchHistory = JSON.parse(
+    localStorage.getItem("searchHistory") || "[]"
+  );
+  searchHistory.push(arg);
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+};
 
 const getImage = () => {
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     container.innerHTML = "";
+    toLocalStorage(input.value);
+    // localStorage.setItem("First", input.value);
+
     fetch(
       `https://api.unsplash.com/search/photos?client_id=u5bAUNTIlopz0D5NOD-mPjOdb-p7SXjvkS_xO76UEvE&query=${input.value}`
     )
