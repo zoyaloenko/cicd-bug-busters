526fa789874720e39a5422110725d82e678a159a
salt-dev <salt-dev@salt-devs-MacBook-Pro-7.local>
2023-02-22 09:52:58 +0100

    small changes

--- src/index.ts
+++ src/index.ts
@@ -9,46 +9,29 @@ input.setAttribute("id", "iid");
 input2.setAttribute("type", "submit");
 input2.setAttribute("value", "Submit");
 
-// Create heading node
-const heading = document.createElement("h1");
-heading.textContent = "It works!";
-
 // Append heading node to the DOM
 const app = document.querySelector("#root");
 app?.append(form);
 form.appendChild(input);
 form.appendChild(input2);
 
+
+
 const getImage = () => {
-
-
-
-
-  input2.addEventListener("submit", () => {
-   
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    fetch(
+      `https://api.unsplash.com/search/photos?client_id=u5bAUNTIlopz0D5NOD-mPjOdb-p7SXjvkS_xO76UEvE&query=${input.value}`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        response.results.forEach((item) => {
+          const imageEl = document.createElement("img");
+          imageEl.setAttribute("src", `${item.urls.thumb}`);
+          app?.appendChild(imageEl);
+        });
+      });
   });
-form.addEventListener("submit", (event)=>{
-  event.preventDefault()
-  
-  fetch(
-    `https://api.unsplash.com/search/photos?client_id=u5bAUNTIlopz0D5NOD-mPjOdb-p7SXjvkS_xO76UEvE&query=${input.value}`
-  ).then(response => response.json())
-  .then(response => {
-    
-
-
-response.results.forEach(item  => {
-  const imageEl = document.createElement("img")
-  imageEl.setAttribute('src',`${item.urls.small}`)
-
-app?.appendChild(imageEl);
-});
-  })
-})
-
-
-
-
 
   // .then(result => console.log(1, result));
   // .then(response => console.log(2, typeof(response)))
@@ -65,18 +48,16 @@ app?.appendChild(imageEl);
 //       .then((response) => {
 //         let data = response.results;
 //         resolve(data);
-        
+
 //       });
 //   });
 // }
-
-
 
 const watchForm = () => {
   input2.addEventListener("submit", () => {
     getImage();
   });
-}; 
+};
 
 watchForm();
 getImage();
